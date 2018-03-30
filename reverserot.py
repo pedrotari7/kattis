8fbe2136c205e28990c69302073720e827b7bcfb
pedrotari7 <pedrotari7@gmail.com>
2018-03-30 17:01:16 +0200

    reverserot

--- reverserot.py
+++ reverserot.py
@@ -3,7 +3,7 @@ import sys
 d = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ_.')
 
 for line in sys.stdin:
-    if line != '0':
+    if line.strip() != '0':
         N = int(line.strip().split()[0])
         s = line.strip().split()[1].upper()
         print ''.join(d[(d.index(c) + N) % len(d)] for c in s[::-1])
